4cc2b6f084406762abf52c660fde469af07b0df8
Artiom <artiom.filkevich@gmail.com>
2020-10-03 19:02:05 +0300

    feat: implement function that decode Morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayOfNumbers = [];
+    // split the string by 10 chars
+    for (let i = 0; i < expr.length; i += 10) {
+        arrayOfNumbers.push(expr.slice(i, i + 10).split(''));
+    }
+
+    let arrayOfMorse = [];
+
+    // convert decimal code into Morse code
+    arrayOfNumbers.forEach(decimalCodeOfLetter => {
+        // check space letter (**********)
+        if (decimalCodeOfLetter == '**********') {
+            arrayOfMorse.push('');
+            return;
+        }
+
+        let oneLetter = '';
+        for (let i = 0; i < decimalCodeOfLetter.length; i += 2) {
+            switch (decimalCodeOfLetter[i]+decimalCodeOfLetter[i+1]) {
+                case '11':
+                    oneLetter += '-';
+                    break;
+                case '10':
+                    oneLetter += '.';
+                    break;
+            }
+        }
+        arrayOfMorse.push(oneLetter);
+    });
+
+    // convert to ordinary letters
+    let resultString = '';
+    arrayOfMorse.forEach(element => {
+        // if we have space letter, add it
+        if (element == '') {
+            resultString += ' ';
+            return;
+        }
+        resultString += MORSE_TABLE[element];
+    });
+
+    return resultString;
 }
 
 module.exports = {
